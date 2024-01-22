4eaa4961b2073c4af6cca5002898854ec9d88539
Ahmad Esam <90632545+Ahmad-Esam12398@users.noreply.github.com>
2024-01-22 22:48:15 +0200

    Afetr Boyhina Mods

--- products module.js
+++ products module.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-//products module.js
-class Product {
-    static lastID = 1;
-    #id; #name; #price; #quantity; #description; #image; #sellerID; #category;#categorypath;
-
-    constructor(id, name, price, quantity, description, image, sellerID, category,categorypath) {
-        this.#id = Product.lastID++;
-        this.#name = name.trim();
-        this.#price = price;
-        this.#quantity = +quantity;
-        this.#description = description.trim();
-        this.#image = image.trim();
-        this.#category = category.trim();
-        this.#sellerID = +sellerID;
-        this.#categorypath= categorypath;
-        
-    }
-    
-    
-    set id(_id){
-        if (id<0){
-            this.#id=0;
-        }
-        else{
-            this.#id=_id;
-        }
-=======
 class Product{
     static lastID = 0;
     static imgPath = "products imgs/"
@@ -38,7 +10,6 @@ class Product{
         this.description = description;
         this.image = Product.imgPath + image;
         this.sellerID = sellerID;
->>>>>>> 578c9dbcb983c9d9440d279ccae8f1a0109b7a84
     }
     set quantity(_quantity){
         if(_quantity < 0){
@@ -88,37 +59,9 @@ class Product{
             this.#sellerID = +_sellerID;
         }
     }
-<<<<<<< HEAD
-    set category(_category){
-        if(_category.trim()==""){
-            this.#category="No catogray";
-        }
-        else {
-            this.#category=_category.trim();
-        }
-    }
-    set categorypath(_categorypath){
-        if(_categorypath.trim()=""){
-            this.#categorypath="NO Catograypath"
-        }
-        else{
-            this.#categorypath=_categorypath.trim()
-        }
-    }
-    get categorypath(){
-        return this.#categorypath;
-    }
     get id(){
         return this.#id;
     }
-    get category(){
-        return this.#category;
-    }
-=======
-    get id(){
-        return this.#id;
-    }
->>>>>>> 578c9dbcb983c9d9440d279ccae8f1a0109b7a84
     get quantity(){
         return this.#quantity;
     }
@@ -137,37 +80,6 @@ class Product{
     get sellerID(){
         return this.#sellerID;
     }
-<<<<<<< HEAD
-    getQuantity() {
-        return this.#quantity;
-    }
-
-    getProductId() {
-        return this.#id;
-    }
-    getProduct() {
-        return {
-          id: this.#id,
-          name: this.#name,
-          price: this.#price,
-          quantity: this.#quantity,
-          description: this.#description,
-          image: this.#image,
-          sellerID: this.#sellerID,
-          category: this.#category,
-          categorypath:this.#categorypath,
-        };
-      }
-      addClickEvent(callback) {
-        const productDiv = document.getElementById(`product-${this.#id}`);
-        if (productDiv) {
-          productDiv.addEventListener('click', () => {
-            callback(this.#id);
-          });
-        }
-      }
-    }
-=======
     getProduct(){
         return {
             id: this.#id,
@@ -188,7 +100,6 @@ class Product{
         this.sellerID = sellerID;
     }
 }
->>>>>>> 578c9dbcb983c9d9440d279ccae8f1a0109b7a84
 export { Product };
 
 
