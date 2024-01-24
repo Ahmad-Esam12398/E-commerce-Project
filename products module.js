50c97509309bff8ef4f0798d72f6988c1a1046e7
Ahmad Esam <90632545+Ahmad-Esam12398@users.noreply.github.com>
2024-01-24 20:06:30 +0200

    latest changes

--- products module.js
+++ products module.js
@@ -1,7 +1,7 @@
 class Product{
     static lastID = 0;
-    #id; #name; #price; #quantity; #description; #image; #sellerID;
-    constructor(name, price, quantity, description, image, sellerID){
+    #id; #name; #price; #quantity; #description; #image; #sellerID; #room;
+    constructor(name, price, quantity, description, image, sellerID, room){
         this.#id = ++Product.lastID;
         this.name = name;
         this.price = price;
@@ -9,6 +9,7 @@ class Product{
         this.description = description;
         this.image = image;
         this.sellerID = sellerID;
+        this.room = room;
     }
     set quantity(_quantity){
         if(_quantity < 0){
@@ -56,6 +57,15 @@ class Product{
         }
         else{
             this.#sellerID = +_sellerID;
+        }
+    }
+    set room(_room){
+        const validRooms = ['Bathroom', 'Bedroom', 'Home Office', 'Kitchen', 'Living Room', 'Cabinet'];
+        if(validRooms.includes(_room)){
+            this.#room = _room;
+        }
+        else{
+            this.#room = 'Other';
         }
     }
     get id(){
